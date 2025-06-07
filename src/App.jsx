0b722d27528a209797e4ca2101686144c6f7f0e6
Santiago Rua <santiruab1@gmail.com>
2025-06-07 12:15:19 -0500

    feat: add routing for Terms, Privacy, and Contact pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { SignInPage } from "./pages/SignInPage"
 import { SignUpPage } from "./pages/SignUpPage"
+import { TermsPage } from "./pages/TermsPage"
+import { PrivacyPage } from "./pages/PrivacyPage"
+import { ContactPage } from "./pages/ContactPage"
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/terms" element={<TermsPage />} />
+      <Route path="/privacy" element={<PrivacyPage />} />
+      <Route path="/contact" element={<ContactPage />} />
     </Routes>
   )
 }
